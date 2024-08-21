4462d8c43ecdfdebf0fd8d81e2b3f3c222dca786
simplycode07 <simplycode07@gmail.com>
2024-08-21 17:08:56 +0530

    updated host

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import csv
-import ast
 
 app = Flask(__name__)
 def read_attendance():
@@ -19,4 +18,4 @@ def attendance():
     return render_template('attendance.html', attendance_data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=3000)
